01d15061a85fb32ae92134bc4c72d22fdbd7a546
jbenavidesnati05 <jorgenati05@gmail.com>
2022-03-21 11:34:58 -0500

    calculadora terminada

--- js/operaciones.js
+++ js/operaciones.js
@@ -18,10 +18,15 @@ function Restar(){
 
 function Multi(){
     let Nr1= parseInt(document.getElementById("nr1").value);
-    let Nr2= parseInt(document.getElementById("nr1").value);
+    let Nr2= parseInt(document.getElementById("nr2").value);
     let multiplicar = Nr1*Nr2;
     document.getElementById('Resultado').value = multiplicar;
+}
 
+function Dividir(){
+    var n1 = parseFloat(document.getElementById("nr1").value);
+    var n2 = parseFloat(document.getElementById("nr2").value);
 
-
+    var dividir = n1/n2;
+    document.getElementById('Resultado').value = dividir;
 }
