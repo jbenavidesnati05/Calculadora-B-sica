c5d228c193168d2e97d96a8a6105841a4151aac1
jbenavidesnati05 <jorgenati05@gmail.com>
2022-03-21 09:54:14 -0500

    subiendo mi calculadora

---
+++ js/cards.js
@@ -0,0 +1,27 @@
+
+    let texto1 = document.getElementById('descripcion');
+    let texto2 = document.getElementById('descripcion');
+    let imagen = document.getElementById('img');
+
+//cargar funciones
+
+
+
+function Cambiartext1(){
+
+    texto2.innerHTML= "Obtén un fantástico desempeño en el juego mientras lo transmites y lo grabas. Disfrute de una experiencia de juego fluida, FPS altos y realismo envolvente con un procesador Intel® Core™ para equipos de desktop";
+    
+}
+
+function Cambiartext2(){
+    texto1.innerHTML="Estos procesadores cuentan con una arquitectura innovadora diseñada para un desempeño inteligente (IA), visualizaciones y gráficos envolventes, y un ajuste y una capacidad de ampliación mejorados que permiten a los gamers y apasionados de las PCs tener el control total de las experiencias en el mundo real.";
+}
+
+function Cambiarimg1(){
+    imagen.src = "img/intel92.jfif";
+}
+
+function Cambiarimg2(){
+   
+    imagen.src = "img/intel9.jfif";
+}
---
+++ js/operaciones.js
@@ -0,0 +1,14 @@
+
+
+// Función procedimiento de Sumar dos nros
+
+function Sumar(){
+    let Nr1 = Number(document.getElementById("nr1").value);
+    let Nr2 = Number(document.getElementById("nr2").value);
+    let sumar = Nr1 + Nr2;
+    document.getElementById('Resultado').value = sumar;
+}
+
+function Restar(){
+    alert('Estoy restando mi sueldo .. ')
+}
